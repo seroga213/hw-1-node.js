2e8dbfb61018fd7a56fc3a566af5d485fbbebb99
Seroga Cherkassov <serogaseroga05@gmail.com>
2023-01-11 14:53:50 +0200

    done

---
+++ contacts.js
@@ -0,0 +1,68 @@
+const fs = require('fs').promises; 
+const path = require('path')
+
+const {v4} = require('uuid')
+
+const contactsPath = path.join(__dirname, 'db/contacts.json')
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+
+    return JSON.parse(data);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const result = contacts.find(item => item.id === String(contactId))
+
+    if (!result){
+      return console.log((`Contact with id = ${contactId} not found!`))
+    } else{
+      return result
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try{
+  const data = await fs.readFile(contactsPath, "utf-8");
+  const parsedContacts = JSON.parse(data);
+  const indx = parsedContacts.findIndex(item => item.id === String(contactId))
+    if (!indx === -1){
+      return console.log("There is no contact with such id")
+    } 
+    const [removeContact] = parsedContacts.splice(indx,1)
+    fs.writeFile(contactsPath, JSON.stringify(parsedContacts))
+    return removeContact
+  } catch(err){
+    console.log(err.message)}
+}
+
+async function addContact(name, email, phone) {
+  try{
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const parsedContacts = JSON.parse(data);
+    const newContacts = { id : v4(), name, email,phone }
+    parsedContacts.push(newContacts)
+    fs.writeFile(contactsPath, JSON.stringify(parsedContacts))
+    
+    return parsedContacts
+
+    } catch(err){
+      console.log(err.message)}
+}
+
+module.exports = {
+  listContacts,
+  addContact,
+  getContactById,
+  removeContact, 
+}
---
+++ index.js
@@ -0,0 +1,41 @@
+const contactsListOperations = require('./contacts')
+
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = await contactsListOperations.listContacts()
+      return console.table(contacts)
+
+    case "get":
+      const contactById = await contactsListOperations.getContactById(id)
+      return console.log(contactById)
+
+    case "add":
+      const newContact = await contactsListOperations.addContact(name,email,phone)
+      return console.log(newContact)
+      
+
+    case "remove":
+      const deletedContact = await contactsListOperations.removeContact(id)
+      return console.log(deletedContact)
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
